394c021160d4b9e6bed5556ab12bf2f1b61e25b2
ivvist <ivvist@gmail.com>
2023-02-24 16:28:10 +0100

    text print

--- tester/tester.go
+++ tester/tester.go
@@ -172,21 +172,6 @@ func (t *Tester) ProcessExcludes(excludes map[string]map[int]shared.ExcludeType)
 	var processed []*cover.Profile
 
 	var p *cover.Profile
-	/*
-		if nil == t.Results {
-			// no excludes in this file - add the profile unchanged
-			var notestdeptblocks []cover.ProfileBlock
-			for fn, v := range excludes {
-				for line, excType := range excludes {
-					notestdeptblocks = append(notestdeptblocks, cover.ProfileBlock{})
-				}
-			}
-			notestdeptprofile := &cover.Profile{
-				FileName: fn,
-			}
-		return nil
-		}
-	*/
 	for _, p = range t.Results {
 
 		// Filenames in t.Results are in go package form. We need to convert to
